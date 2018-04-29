e7af56615f7ddacd4dcaff09e66c8558cdc3c707
Martin Zackrisson <martin.zackrisson@gmail.com>
2018-04-29 12:54:02 +0200

    Fixed error in api

--- app/api.py
+++ app/api.py
@@ -93,7 +93,7 @@ def add_api(app):
             scores[score_type].update(
                 count=len(ranked_highscores),
                 type=score_type,
-                name=name,
+                name=score_settings.get('name', score_type.capitalize()),
                 score_name=score_settings.get("scoreName", "Score"),
             )
             all_highscores[score_type] = ranked_highscores[:count]
